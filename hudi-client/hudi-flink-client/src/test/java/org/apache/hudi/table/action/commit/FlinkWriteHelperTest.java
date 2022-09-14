b6377d843ce6bd984818afab557ded9f826a3ebc
XuQianJin-Stars <forwardxu@apache.com>
2022-09-14 09:59:29 +0800

    fixed ordering field null logical

--- hudi-client/hudi-flink-client/src/test/java/org/apache/hudi/table/action/commit/FlinkWriteHelperTest.java
+++ hudi-client/hudi-flink-client/src/test/java/org/apache/hudi/table/action/commit/FlinkWriteHelperTest.java
@@ -1,16 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hudi.table.action.commit;
 
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.hudi.avro.HoodieAvroUtils;
 import org.apache.hudi.common.model.HoodieAvroRecord;
 import org.apache.hudi.common.model.HoodieKey;
 import org.apache.hudi.common.model.HoodieOperation;
 import org.apache.hudi.common.model.PartialUpdateAvroPayload;
 import org.apache.hudi.index.HoodieIndex;
-
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.flink.configuration.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -30,7 +46,7 @@ public class FlinkWriteHelperTest {
 
   private transient Schema avroSchema;
 
-  private String preCombineFields  = "";
+  private String preCombineFields = "";
 
   public static final String SCHEMA = "{\n"
       + "  \"type\": \"record\",\n"
@@ -66,14 +82,15 @@ public class FlinkWriteHelperTest {
     AtomicInteger fbCnt = new AtomicInteger();
     List<GenericRecord> records = new ArrayList<>();
     for (int i = 0; i < 100; i++) {
+      long ts = System.currentTimeMillis();
       GenericRecord row1 = new GenericData.Record(this.avroSchema);
       row1.put("id", "jack");
       row1.put("fa", faCnt.getAndIncrement() + "");
-      row1.put("_ts1", System.currentTimeMillis());
+      row1.put("_ts1", ts);
       GenericRecord row2 = new GenericData.Record(this.avroSchema);
       row2.put("id", "jack");
       row2.put("fb", fbCnt.getAndIncrement() + "");
-      row2.put("_ts2", System.currentTimeMillis());
+      row2.put("_ts2", ts);
       records.add(row1);
       records.add(row2);
     }
@@ -81,8 +98,9 @@ public class FlinkWriteHelperTest {
     return records.stream().map(genericRowData -> {
       try {
         String orderingFieldValText = HoodieAvroUtils.getMultipleNestedFieldVals(genericRowData,
-              preCombineFields, false).toString();
-        return new HoodieAvroRecord(new HoodieKey("1", "default"), new PartialUpdateAvroPayload(genericRowData, orderingFieldValText), HoodieOperation.INSERT);
+            preCombineFields, false).toString();
+        return new HoodieAvroRecord(new HoodieKey("1", "default"),
+            new PartialUpdateAvroPayload(genericRowData, orderingFieldValText), HoodieOperation.INSERT);
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
